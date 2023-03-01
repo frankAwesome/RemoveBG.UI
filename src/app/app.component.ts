b1233c278695702dad6bc703bcc730ec994adc88
Francois de Klerk <francois@Francoiss-MacBook-Air.local>
2023-03-01 14:44:44 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
+
+  constructor(private http: HttpClient, private spinner: NgxSpinnerService)
+  {
+  }
+
   title = 'RemoveBG';
+
+  imageSrc = '';
+  imageList: string[] = [];
+  hideDropBox: boolean = true;
+  isLoading: boolean = false;
+
+
+  onDrop(event: any) {
+    this.isLoading = true;
+    
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+    this.hideDropBox = false;
+    this.handleFiles(files);
+  }
+
+  onDragOver(event: any) {
+    event.preventDefault();
+  }
+
+  onFileSelected(event: any) {
+    const files = event.target.files;
+    this.handleFiles(files);
+  }
+
+  handleFiles(files: FileList) {
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.spinner.show();
+        // this.imageList.push(e.target.result);
+
+        const base64Image = e.target.result.toString().split(',')[1];
+        this.http.post<any>('http://localhost:5000/removebg', base64Image ).subscribe((response: any) => {
+          console.log(response);
+          this.imageList.push('data:image/jpeg;base64,' + response.image);
+
+          this.spinner.hide();
+        });
+      };
+      reader.readAsDataURL(file);
+      
+    }
+  }
+
+  downloadImage(image: string) {
+    const link = document.createElement('a');
+    link.setAttribute('href', image);
+    link.setAttribute('download', 'image.png');
+    link.style.display = 'none';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+
+interface NgxSpinnerConfig {
+  type?: string;
+}
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    NgxSpinnerModule.forRoot({
+      type: 'ball-scale-multiple'
+    }),
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
